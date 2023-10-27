879331162b9e0c2039931205559716e4cea1c5d7
Cyrus Waithaka <cygawa@gmail.com>
2023-10-27 13:53:33 +0300

    Rename degree lable

--- models/academic_qualifications.py
+++ models/academic_qualifications.py
@@ -6,8 +6,8 @@ class AcademicQualification(models.Model):
     _description = 'Academic Qualification'
 
     human_resource_id = fields.Many2one('human.resource', string='Resource', required=True)
-    degree = fields.Char(string='Degree', required=True)
-    institution = fields.Char(string='Institution', required=True)
+    degree = fields.Char(string='Certification', required=True)  # ToDo this should be a many2one to registered institutions
+    institution = fields.Char(string='Institution', required=True)  #ToDo this should be a many2one to registered institutions
     # start_date = fields.Date(string='Admission Date', required=True)
     # completion_date = fields.Date(string='Completion Date ', required=True)
     year = fields.Char(string='Year', required=True)
